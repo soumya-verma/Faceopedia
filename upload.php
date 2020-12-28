00c569453fc227bbeccf43418051e2de10891069
Soumya Verma <soumya.verma2000@gmail.com>
2020-12-28 19:13:55 +0530

    Added html and css files

--- upload.php
+++ upload.php
@@ -4,24 +4,24 @@
 	<title>Faceopedia - Add New</title>
 </head>
 <body>
-	<div style="padding-top: 3%; padding-left: 10%;">
 	<form action="uploadimage.php" method="POST" enctype="multipart/form-data">
 		<p>Name :</p>
-		<input type="text" name="name" size="42">
+		<input type="text" name="name">
 		<p>Label :</p>
-		<input type="text" name="label" size="42">
+		<input type="text" name="label">
 		<p>Date of Birth :</p>
 		<input type="date" name="dob">
 		<p>Profession :</p>
-		<input type="text" name="prof" size="42">
+		<input type="text" name="prof">
 		<p>Details :</p>
-		<input type="text" name="detail" style="height:100px;width:300px;">
+		<input type="text" name="detail">
+		<p>Information :</p>
+		<input type="text" name="info">
 		<p>Add Link :</p>
-		<input type="text" name="link" size="42">
+		<input type="text" name="link">
 		<p>Add Image :</p>
 		<input type="file" name="file">
 		<button type="submit" name="submit">UPLOAD</button>
 	</form>
-</div>
 </body>
 </html>
